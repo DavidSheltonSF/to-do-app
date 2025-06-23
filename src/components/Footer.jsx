cc87e200a7d334b7b1cee428711aa6a36abb1290
DavidSheltonSF <david.faria012@gmail.com>
2025-06-23 13:46:08 -0300

    styles(Footer): format code with prettier.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
-import styles from "./footer.module.css"
+import styles from './footer.module.css';
 
 export default function Footer({ doneTodos, totalTodos }) {
-  return <footer className={styles.Footer}>
-    <span>Completed: {doneTodos}</span>
-    <span>Total: {totalTodos}</span>
-  </footer>
+  return (
+    <footer className={styles.Footer}>
+      <span>Completed: {doneTodos}</span>
+      <span>Total: {totalTodos}</span>
+    </footer>
+  );
 }
