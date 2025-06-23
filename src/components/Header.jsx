784c7ca795e43d94893cf72f37bd754496194083
DavidSheltonSF <david.faria012@gmail.com>
2025-06-23 15:05:00 -0300

    style:(Header): rename  heaner css class to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import styles from './header.module.css';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
+    <header className={styles.Header}>
       <span>My Todo List</span>
     </header>
   );
